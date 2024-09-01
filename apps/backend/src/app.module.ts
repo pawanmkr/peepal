af8637c79f5e575247303823a73f63456e6b06bd
Pawan Kumar <iampawanmkr@gmail.com>
2024-09-01 13:43:45 +0530

    Modules generated, ENV setup

---
+++ apps/backend/src/app.module.ts
@@ -0,0 +1,54 @@
+import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+
+import { AppController } from './app.controller';
+import { env } from './config/env.config';
+import chalk from 'chalk';
+
+// // Workaround for dynamic import
+async function getChalk(): Promise<typeof chalk> {
+	const module = await (eval(`import('chalk')`) as Promise<any>);
+	return module.default;
+}
+
+@Module({
+	imports: [
+		SequelizeModule.forRootAsync({
+			useFactory: () => ({
+				dialect: 'postgres',
+				host: env.DB_HOST,
+				username: env.DB_USER,
+				password: env.DB_PASSWORD,
+				database: env.DB_NAME,
+				dialectOptions: {
+					ssl: {
+						require: true,
+						rejectUnauthorized: false,
+					},
+				},
+				models: [],
+				define: {
+					underscored: true,
+					timestamps: true,
+					freezeTableName: true,
+					deletedAt: true,
+					paranoid: true,
+				},
+				logQueryParameters: true,
+				benchmark: true,
+				logging: async (sql, timing) => {
+					const chalk = await getChalk();
+					console.log(chalk.green(`\nElapsed Time: ${timing}ms`));
+					console.log(chalk.dim(sql));
+				},
+				autoLoadModels: true,
+				synchronize: true, // WARNING: Don't make it true in production
+				retryAttempts: 3,
+				retryDelay: 10000,
+			}),
+		}),
+	],
+	controllers: [AppController],
+	providers: [],
+})
+export class AppModule { }
